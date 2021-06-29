9c80b42f65ebe6da7a8edbeb091e84d76d56775e
Tadas Danielius <tadasdanielius@users.noreply.github.com>
2021-06-29 13:11:00 +0300

    updated patch to copy train.py

--- patch.sh
+++ patch.sh
@@ -3,3 +3,6 @@
 echo "Patching framework"
 echo "Copying file coco.py"
 cp /patches/coco.py /tensorpack/examples/FasterRCNN/dataset/coco.py
+
+echo "Copying file train.py"
+cp /patches/train.py /tensorpack/examples/FasterRCNN/train.py
